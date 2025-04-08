d6c7a70cce9e8fde5871b81b1b30f0011e275f78
kram1k <7685181@gmail.com>
2025-04-08 16:56:14 +0300

    clining: `isort` & `black`

--- testvoe/events/management/commands/parsing.py
+++ testvoe/events/management/commands/parsing.py
@@ -1,5 +1,5 @@
-import os
 import json
+import os
 
 from django.core.management.base import BaseCommand, CommandError
 
@@ -8,8 +8,20 @@ class Command(BaseCommand):
     help = "The command simplifies loading data from JSON for Django ORM operation"
 
     def add_arguments(self, parser):
-        parser.add_argument("--new", "-n", type=str, required=True, help="Path to the new data file (output)")
-        parser.add_argument("--old", "-o", type=str, required=True, help="Path to the old data file (input)")
+        parser.add_argument(
+            "--new",
+            "-n",
+            type=str,
+            required=True,
+            help="Path to the new data file (output)",
+        )
+        parser.add_argument(
+            "--old",
+            "-o",
+            type=str,
+            required=True,
+            help="Path to the old data file (input)",
+        )
 
     def handle(self, *args, **options):
         new_file_path = options["new"]
@@ -34,15 +46,21 @@ class Command(BaseCommand):
                             "region": obj["data"]["region"],
                             "address": obj["data"]["address"],
                             "year": obj["data"]["year"],
-                            "inter_budget_transfer_amount": obj["data"]["inter_budget_transfer_amount"]
-                        }
+                            "inter_budget_transfer_amount": obj["data"][
+                                "inter_budget_transfer_amount"
+                            ],
+                        },
                     }
                     content.append(info)
 
             with open(new_file_path, "w", encoding="utf8") as new_file:
                 json.dump(content, new_file, indent=4)
 
-            self.stdout.write(self.style.SUCCESS(f"Successfully simplifies content from '{old_file_path}' to '{new_file_path}'"))
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Successfully simplifies content from '{old_file_path}' to '{new_file_path}'"
+                )
+            )
 
         except Exception as e:
             raise CommandError(f"An error occurred: {str(e)}")
